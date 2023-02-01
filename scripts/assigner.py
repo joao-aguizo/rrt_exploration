b6c7a7f7fc31aeafc9af2662dc39d4ef190e6199
João Aguizo <joao@ingeniarius.pt>
2023-02-01 17:54:14 +0000

    Single unit assigner node. Management of multi-robot can be done on sepparate node.

--- scripts/assigner.py
+++ scripts/assigner.py
@@ -3,47 +3,54 @@
 #--------Include modules---------------
 from copy import copy
 import rospy
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Point
 from nav_msgs.msg import OccupancyGrid
-import tf
 from rrt_exploration.msg import PointArray
-from time import time
 from numpy import array
-from numpy import linalg as LA
-from numpy import all as All
-from numpy import inf
 from functions import robot, informationGain, discount
 from numpy.linalg import norm
-
-# Subscribers' callbacks------------------------------
-mapData = OccupancyGrid()
-frontiers = []
-global1 = OccupancyGrid()
-global2 = OccupancyGrid()
-global3 = OccupancyGrid()
-globalmaps = []
+from enum import Enum
 
 
-def callBack(data):
-    global frontiers
-    frontiers = []
+# robot status enumeration
+class Status(Enum):
+    IDLE = 0
+    BUSY = 1
+
+
+# node global variables
+_map = OccupancyGrid()
+_frontiers = []
+_status = Status.IDLE
+_revenue = float('-inf')
+
+
+def points_cb(data):
+    global _frontiers
+    _frontiers = []
     for point in data.points:
-        frontiers.append(array([point.x, point.y]))
+        _frontiers.append(array([point.x, point.y]))
 
 
-def mapCallBack(data):
-    global mapData
-    mapData = data
-# Node----------------------------------------------
+def map_cb(data):
+    global _map
+    _map = data
+
+
+def active_cb():
+    global _status
+    _status = Status.BUSY
+
+
+def done_cb(status, result):
+    global _status
+    _status = Status.IDLE
 
 
 def node():
-    global frontiers, mapData, global1, global2, global3, globalmaps
-    rospy.init_node('assigner', anonymous=False, log_level=rospy.WARN)
+    global _frontiers, _map, _status, _revenue
+    rospy.init_node('assigner', anonymous=False, log_level=rospy.INFO)
 
     # fetching all parameters
-    map_topic = rospy.get_param('~map_topic', '/map')
     # this can be smaller than the laser scanner range, >> smaller >>less computation time>> too small is not good, info gain won't be accurate
     info_radius = rospy.get_param('~info_radius', 1.0)
     info_multiplier = rospy.get_param('~info_multiplier', 3.0)
@@ -51,117 +58,78 @@ def node():
     hysteresis_radius = rospy.get_param('~hysteresis_radius', 3.0)
     # bigger than 1 (biase robot to continue exploring current region
     hysteresis_gain = rospy.get_param('~hysteresis_gain', 2.0)
-    frontiers_topic = rospy.get_param('~frontiers_topic', '/filtered_points')
-    n_robots = rospy.get_param('~n_robots', 1)
-    namespace = rospy.get_param('~namespace', '')
-    namespace_init_count = rospy.get_param('namespace_init_count', 1)
     delay_after_assignement = rospy.get_param('~delay_after_assignement', 0.5)
-    rateHz = rospy.get_param('~rate', 100)
+    rate = rospy.Rate(rospy.get_param('~rate', 100))
 
-    rate = rospy.Rate(rateHz)
-#-------------------------------------------
-    rospy.Subscriber(map_topic, OccupancyGrid, mapCallBack)
-    rospy.Subscriber(frontiers_topic, PointArray, callBack)
-#---------------------------------------------------------------------------------------------------------------
-    rospy.logwarn("len(frontiers): {}".format(len(frontiers)))
-    
-    r = rospy.Rate(1)
-    # wait if no frontier is received yet
-    while len(frontiers) < 1:
-        r.sleep()
+    rospy.Subscriber("map", OccupancyGrid, map_cb)
+    rospy.Subscriber("filtered_points", PointArray, points_cb)
 
-    centroids = copy(frontiers)
-    
-    rospy.logwarn("len(centroids): {}".format(len(centroids)))
-    #wait if map is not received yet
-    while (len(mapData.data) < 1):
-        r.sleep()
+    # robot helper class
+    bot = robot()
 
-    robots = []
-    robots.append(robot())
-    for i in range(0, n_robots):
-        robots[i].sendGoal(robots[i].getPosition())
-   
-    rospy.loginfo("robots: {}".format(robots))
-#-------------------------------------------------------------------------
-#---------------------     Main   Loop     -------------------------------
-#-------------------------------------------------------------------------
     while not rospy.is_shutdown():
-        centroids = copy(frontiers)
-#-------------------------------------------------------------------------
-#Get information gain for each frontier point
+        rate.sleep()
+
+        # jump iteration if while no data is available
+        if len(_frontiers) < 1 or len(_map.data) < 1:
+            continue
+
+        centroids = copy(_frontiers)
+
+        # get information gain for each frontier point
         infoGain = []
         for ip in range(0, len(centroids)):
             infoGain.append(informationGain(
-                mapData, [centroids[ip][0], centroids[ip][1]], info_radius))
-#-------------------------------------------------------------------------
-#get number of available/busy robots
-        na = []  # available robots
-        nb = []  # busy robots
-        for i in range(0, n_robots):
-            if (robots[i].getState() == 1):
-                nb.append(i)
-            else:
-                na.append(i)
-        rospy.loginfo("available robots: "+str(na))
-#-------------------------------------------------------------------------
-#get dicount and update informationGain
-        for i in nb+na:
-            infoGain = discount(
-                mapData, robots[i].assigned_point, centroids, infoGain, info_radius)
-#-------------------------------------------------------------------------
+                _map, [centroids[ip][0], centroids[ip][1]], info_radius))
+
+        # get dicount and update informationGain
+        infoGain = discount(
+            _map, bot.assigned_point, centroids, infoGain, info_radius)
+
+        rospy.logdebug("infoGain: {}".format(infoGain))
+
         revenue_record = []
         centroid_record = []
-        id_record = []
-
-        for ir in na:
+        if _status is Status.IDLE:
             for ip in range(0, len(centroids)):
-                cost = norm(robots[ir].getPosition()-centroids[ip])
-                threshold = 1
+                cost = norm(bot.getPosition()-centroids[ip])
                 information_gain = infoGain[ip]
-                if (norm(robots[ir].getPosition()-centroids[ip]) <= hysteresis_radius):
 
+                if cost <= hysteresis_radius:
                     information_gain *= hysteresis_gain
+
                 revenue = information_gain*info_multiplier-cost
                 revenue_record.append(revenue)
                 centroid_record.append(centroids[ip])
-                id_record.append(ir)
 
-        if len(na) < 1:
-            revenue_record = []
-            centroid_record = []
-            id_record = []
-            for ir in nb:
-                for ip in range(0, len(centroids)):
-                    cost = norm(robots[ir].getPosition()-centroids[ip])
-                    threshold = 1
-                    information_gain = infoGain[ip]
-                    if (norm(robots[ir].getPosition()-centroids[ip]) <= hysteresis_radius):
-                        information_gain *= hysteresis_gain
+        elif _status is Status.BUSY:
+            for ip in range(0, len(centroids)):
+                cost = norm(bot.getPosition()-centroids[ip])
+                information_gain = infoGain[ip]
 
-                    if ((norm(centroids[ip]-robots[ir].assigned_point)) < hysteresis_radius):
-                        information_gain = informationGain(
-                            mapData, [centroids[ip][0], centroids[ip][1]], info_radius)*hysteresis_gain
+                if cost <= hysteresis_radius:
+                    information_gain *= hysteresis_gain
 
-                    revenue = information_gain*info_multiplier-cost
-                    revenue_record.append(revenue)
-                    centroid_record.append(centroids[ip])
-                    id_record.append(ir)
+                if norm(centroids[ip]-bot.assigned_point) < hysteresis_radius:
+                    information_gain = informationGain(
+                        _map, [centroids[ip][0], centroids[ip][1]], info_radius)*hysteresis_gain
 
-        rospy.loginfo("revenue record: "+str(revenue_record))
-        rospy.loginfo("centroid record: "+str(centroid_record))
-        rospy.loginfo("robot IDs record: "+str(id_record))
+                revenue = information_gain*info_multiplier-cost
+                revenue_record.append(revenue)
+                centroid_record.append(centroids[ip])
 
-#-------------------------------------------------------------------------
-        if (len(id_record) > 0):
-            winner_id = revenue_record.index(max(revenue_record))
-            robots[id_record[winner_id]].sendGoal(centroid_record[winner_id])
-            rospy.loginfo(namespace+str(namespace_init_count +
-                          id_record[winner_id])+"  assigned to  "+str(centroid_record[winner_id]))
-            rospy.sleep(delay_after_assignement)
-#-------------------------------------------------------------------------
-        rate.sleep()
-#-------------------------------------------------------------------------
+        # rospy.loginfo("revenue record: "+ str(revenue_record))
+        # rospy.loginfo("centroid record: "+ str(centroid_record))
+        # rospy.loginfo("robot IDs record: " + str(id_record))
+        winner_revenue = max(revenue_record)
+
+        rospy.loginfo("status: {} | gain: {} | cost: {} | winner: {}".format(
+            str(_status), information_gain, cost, winner_revenue))
+
+        winner_revenue_id = revenue_record.index(winner_revenue)
+        bot.sendGoal(
+            centroid_record[winner_revenue_id], active_cb=active_cb, done_cb=done_cb)
+        rospy.sleep(delay_after_assignement)
 
 
 if __name__ == '__main__':
