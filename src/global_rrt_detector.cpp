94bdf94058bf979e1346325d8838acdeedd8d65f
João Aguizo <joao@ingeniarius.pt>
2023-02-02 14:50:31 +0000

    WIP: Review of rrt detectors.

--- src/global_rrt_detector.cpp
+++ src/global_rrt_detector.cpp
@@ -14,18 +14,18 @@
 #include "nav_msgs/MapMetaData.h"
 #include "geometry_msgs/Point.h"
 #include "visualization_msgs/Marker.h"
+#include "visualization_msgs/MarkerArray.h"
 #include <tf/transform_listener.h>
 
 // global variables
 nav_msgs::OccupancyGrid mapData;
-geometry_msgs::PointStamped clickedpoint;
 geometry_msgs::PointStamped exploration_goal;
-visualization_msgs::Marker points, line;
 float xdim, ydim, resolution, Xstartx, Xstarty, init_map_x, init_map_y;
+std::vector<geometry_msgs::Point> userDefinedFrontiers;
+bool userDefinedFrontiersInitialized = false;
+rdm r; // for generating random numbers
+visualization_msgs::Marker lines, points;
 
-rdm r; // for genrating random numbers
-
-// Subscribers callback functions---------------------------------------
 void mapCallBack(const nav_msgs::OccupancyGrid::ConstPtr &msg)
 {
 	mapData = *msg;
@@ -33,13 +33,14 @@ void mapCallBack(const nav_msgs::OccupancyGrid::ConstPtr &msg)
 
 void rvizCallBack(const geometry_msgs::PointStamped::ConstPtr &msg)
 {
+	if (userDefinedFrontiers.size() > 4)
+	{
+		userDefinedFrontiersInitialized = false;
+		userDefinedFrontiers.clear();
+		ROS_INFO("Restart user defined frontier acquisition through RVIZ.");
+	}
 
-	geometry_msgs::Point p;
-	p.x = msg->point.x;
-	p.y = msg->point.y;
-	p.z = msg->point.z;
-
-	points.points.push_back(p);
+	userDefinedFrontiers.push_back(msg->point);
 }
 
 int main(int argc, char **argv)
@@ -56,171 +57,155 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "global_rrt_frontier_detector");
 	ros::NodeHandle nh;
 
-	// fetching all parameters
-	float eta, init_map_x, init_map_y, range;
-	std::string map_topic, base_frame_topic;
+	// parameters variables
+	float rateHz, eta;
 
-	std::string ns;
-	ns = ros::this_node::getName();
+	// parameters
+	ros::param::param<float>("~eta", eta, 0.5);
+	ros::param::param<float>("~rate", rateHz, 100);
 
-	ros::param::param<float>(ns + "/eta", eta, 0.5);
-	ros::param::param<std::string>(ns + "/map_topic", map_topic, "/robot_1/map");
-	//---------------------------------------------------------------
-	ros::Subscriber sub = nh.subscribe(map_topic, 100, mapCallBack);
+	// subscribers
+	ros::Subscriber map_sub = nh.subscribe("map", 100, mapCallBack);
 	ros::Subscriber rviz_sub = nh.subscribe("/clicked_point", 100, rvizCallBack);
 
-	ros::Publisher targetspub = nh.advertise<geometry_msgs::PointStamped>("/detected_points", 10);
-	ros::Publisher pub = nh.advertise<visualization_msgs::Marker>(ns + "_shapes", 10);
+	// publishers
+	ros::Publisher point_pub = nh.advertise<geometry_msgs::PointStamped>("detected_points", 10);
+	ros::Publisher marker_pub = nh.advertise<visualization_msgs::MarkerArray>(ros::this_node::getName() + "_shapes", 10);
 
-	ros::Rate rate(100);
-
-	// wait until map is received, when a map is received, mapData.header.seq will not be < 1
-	while (mapData.header.seq < 1 or mapData.data.size() < 1)
-	{
-		ros::spinOnce();
-		ros::Duration(0.1).sleep();
-	}
-
-	// visualizations  points and lines..
-	points.header.frame_id = mapData.header.frame_id;
-	line.header.frame_id = mapData.header.frame_id;
-	points.header.stamp = ros::Time(0);
-	line.header.stamp = ros::Time(0);
-
-	points.ns = line.ns = "markers";
-	points.id = 0;
-	line.id = 1;
-
-	points.type = points.POINTS;
-	line.type = line.LINE_LIST;
-
-	// Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-	points.action = points.ADD;
-	line.action = line.ADD;
-	points.pose.orientation.w = 1.0;
-	line.pose.orientation.w = 1.0;
-	line.scale.x = 0.03;
-	line.scale.y = 0.03;
-	points.scale.x = 0.3;
-	points.scale.y = 0.3;
-
-	line.color.r = 9.0 / 255.0;
-	line.color.g = 91.0 / 255.0;
-	line.color.b = 236.0 / 255.0;
-	points.color.r = 255.0 / 255.0;
-	points.color.g = 0.0 / 255.0;
-	points.color.b = 0.0 / 255.0;
-	points.color.a = 1.0;
-	line.color.a = 1.0;
-	points.lifetime = ros::Duration();
-	line.lifetime = ros::Duration();
-
-	geometry_msgs::Point p;
-
-	while (points.points.size() < 5)
-	{
-		ros::spinOnce();
-
-		pub.publish(points);
-	}
-
-	std::vector<float> temp1;
-	temp1.push_back(points.points[0].x);
-	temp1.push_back(points.points[0].y);
-
-	std::vector<float> temp2;
-	temp2.push_back(points.points[2].x);
-	temp2.push_back(points.points[0].y);
-
-	init_map_x = Norm(temp1, temp2);
-	temp1.clear();
-	temp2.clear();
-
-	temp1.push_back(points.points[0].x);
-	temp1.push_back(points.points[0].y);
-
-	temp2.push_back(points.points[0].x);
-	temp2.push_back(points.points[2].y);
-
-	init_map_y = Norm(temp1, temp2);
-	temp1.clear();
-	temp2.clear();
-
-	Xstartx = (points.points[0].x + points.points[2].x) * .5;
-	Xstarty = (points.points[0].y + points.points[2].y) * .5;
-
-	geometry_msgs::Point trans;
-	trans = points.points[4];
-	std::vector<std::vector<float>> V;
-	std::vector<float> xnew;
-	xnew.push_back(trans.x);
-	xnew.push_back(trans.y);
-	V.push_back(xnew);
-
-	points.points.clear();
-	pub.publish(points);
-
-	std::vector<float> frontiers;
-	int i = 0;
-	float xr, yr;
+	// algorithm variables
+	float xr, yr, init_map_x, init_map_y, range;
 	std::vector<float> x_rand, x_nearest, x_new;
+	std::vector<std::vector<float>> V;
 
-	// Main loop
+	ros::Rate rate(rateHz);
 	while (ros::ok())
 	{
-
-		// Sample free
-		x_rand.clear();
-		xr = (drand() * init_map_x) - (init_map_x * 0.5) + Xstartx;
-		yr = (drand() * init_map_y) - (init_map_y * 0.5) + Xstarty;
-
-		x_rand.push_back(xr);
-		x_rand.push_back(yr);
-
-		// Nearest
-		x_nearest = Nearest(V, x_rand);
-
-		// Steer
-
-		x_new = Steer(x_nearest, x_rand, eta);
-
-		// ObstacleFree    1:free     -1:unkown (frontier region)      0:obstacle
-		char checking = ObstacleFree(x_nearest, x_new, mapData);
-
-		if (checking == -1)
-		{
-			exploration_goal.header.stamp = ros::Time(0);
-			exploration_goal.header.frame_id = mapData.header.frame_id;
-			exploration_goal.point.x = x_new[0];
-			exploration_goal.point.y = x_new[1];
-			exploration_goal.point.z = 0.0;
-			p.x = x_new[0];
-			p.y = x_new[1];
-			p.z = 0.0;
-			points.points.push_back(p);
-			pub.publish(points);
-			targetspub.publish(exploration_goal);
-			points.points.clear();
-		}
-
-		else if (checking == 1)
-		{
-			V.push_back(x_new);
-
-			p.x = x_new[0];
-			p.y = x_new[1];
-			p.z = 0.0;
-			line.points.push_back(p);
-			p.x = x_nearest[0];
-			p.y = x_nearest[1];
-			p.z = 0.0;
-			line.points.push_back(p);
-		}
-
-		pub.publish(line);
-
 		ros::spinOnce();
 		rate.sleep();
+
+		// jump iteration if no data is available
+		if (mapData.header.seq < 1 || mapData.data.size() < 1)
+		{
+			continue;
+		}
+
+		visualization_msgs::MarkerArray marker_array;
+
+		// visualization markers setup
+		points.header.frame_id = mapData.header.frame_id;
+		points.ns = "points";
+		points.type = points.POINTS;
+		points.action = points.ADD;
+		points.pose.orientation.w = 1.0;
+		points.scale.x = 0.3;
+		points.scale.y = 0.3;
+		points.color.r = 255.0 / 255.0;
+		points.color.g = 0.0 / 255.0;
+		points.color.b = 0.0 / 255.0;
+		points.color.a = 1.0;
+
+		auto tmp = lines.points;
+		lines = points;
+		lines.points = tmp;
+		lines.ns = "lines";
+		lines.type = lines.LINE_LIST;
+		lines.action = lines.ADD;
+		lines.scale.x = 0.03;
+		lines.scale.y = 0.03;
+		lines.color.r = 9.0 / 255.0;
+		lines.color.g = 91.0 / 255.0;
+		lines.color.b = 236.0 / 255.0;
+
+		// user defined area was set and system was not yet initialized
+		if (userDefinedFrontiers.size() > 4 && !userDefinedFrontiersInitialized)
+		{
+			ROS_INFO_ONCE("Initializing...");
+
+			std::vector<float> tmp_1, tmp_2, tmp_3;
+			tmp_1.push_back(userDefinedFrontiers[0].x);
+			tmp_1.push_back(userDefinedFrontiers[0].y);
+
+			tmp_2.push_back(userDefinedFrontiers[2].x);
+			tmp_2.push_back(userDefinedFrontiers[0].y);
+
+			init_map_x = Norm(tmp_1, tmp_2);
+			tmp_1.clear();
+			tmp_2.clear();
+
+			tmp_1.push_back(userDefinedFrontiers[0].x);
+			tmp_1.push_back(userDefinedFrontiers[0].y);
+
+			tmp_2.push_back(userDefinedFrontiers[0].x);
+			tmp_2.push_back(userDefinedFrontiers[2].y);
+
+			init_map_y = Norm(tmp_1, tmp_2);
+			tmp_1.clear();
+			tmp_2.clear();
+
+			Xstartx = (userDefinedFrontiers[0].x + userDefinedFrontiers[2].x) * .5;
+			Xstarty = (userDefinedFrontiers[0].y + userDefinedFrontiers[2].y) * .5;
+
+			tmp_3.push_back(userDefinedFrontiers[4].x);
+			tmp_3.push_back(userDefinedFrontiers[4].y);
+			V.push_back(tmp_3);
+
+			userDefinedFrontiersInitialized = true;
+		}
+		else if (userDefinedFrontiersInitialized)
+		{
+			ROS_INFO_ONCE("Generating trees...");
+
+			// Sample free
+			x_rand.clear();
+			xr = (drand() * init_map_x) - (init_map_x * 0.5) + Xstartx;
+			yr = (drand() * init_map_y) - (init_map_y * 0.5) + Xstarty;
+
+			x_rand.push_back(xr);
+			x_rand.push_back(yr);
+
+			// Nearest
+			x_nearest = Nearest(V, x_rand);
+
+			// Steer
+			x_new = Steer(x_nearest, x_rand, eta);
+
+			// ObstacleFree    1:free     -1:unkown (frontier region)      0:obstacle
+			char checking = ObstacleFree(x_nearest, x_new, mapData);
+
+			geometry_msgs::Point tmp;
+			switch (checking)
+			{
+			case -1:
+				exploration_goal.header.frame_id = mapData.header.frame_id;
+				exploration_goal.point.x = x_new[0];
+				exploration_goal.point.y = x_new[1];
+				exploration_goal.point.z = 0.0;
+				tmp.x = x_new[0];
+				tmp.y = x_new[1];
+				tmp.z = 0.0;
+				points.points.push_back(tmp);
+				point_pub.publish(exploration_goal);
+				break;
+			case 1:
+				V.push_back(x_new);
+				tmp.x = x_new[0];
+				tmp.y = x_new[1];
+				tmp.z = 0.0;
+				lines.points.push_back(tmp);
+				tmp.x = x_nearest[0];
+				tmp.y = x_nearest[1];
+				tmp.z = 0.0;
+				lines.points.push_back(tmp);
+				break;
+			}
+
+			// update publish tree's markers array
+			marker_array.markers.push_back(lines);
+			marker_array.markers.push_back(points);
+			marker_pub.publish(marker_array);
+
+			points.points.clear();
+		}
 	}
 	return 0;
 }
